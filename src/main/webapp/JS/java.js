7d738868396d91c42b6cf0a653cd2d7e23b389e1
alvaro11rf <alvaro11rf@gmail.com>
2023-11-08 19:41:34 +0100

    second commit

--- src/main/webapp/JS/java.js
+++ src/main/webapp/JS/java.js
@@ -1,58 +1,58 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const canvas = document.getElementById("canvas");
+    if (canvas) {
+        const ctx = canvas.getContext("2d");
 
+        const brushCheckbox = document.getElementById("brush");
+        const shapeSizeInput = document.getElementById("size");
+        const colorInput = document.getElementById("color");
+        const clearCanvasButton = document.getElementById("clearCanvas");
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+        let isDrawing = false;
 
-const brushSizeInput = document.getElementById("size");
-const shapeSizeInput = document.getElementById("size");
-const colorInput = document.getElementById("color");
-const clearCanvasButton = document.getElementById("clearCanvas");
+        // Agrega un evento de escucha al cambio en el estado de la casilla de verificación
+        brushCheckbox.addEventListener("change", () => {
+            isDrawing = brushCheckbox.checked; // Habilita o deshabilita el dibujo a mano alzada
+        });
 
-let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
+        // Variables para el inicio y fin del trazo
+        let lastX, lastY;
 
-canvas.addEventListener("mousedown", () => {
-    isDrawing = true;
-    [lastX, lastY] = [event.offsetX, event.offsetY];
-});
+        // Agrega un evento de escucha para el movimiento del mouse en el lienzo
+        canvas.addEventListener("mousedown", (event) => {
+            isDrawing = true;
+            [lastX, lastY] = [event.offsetX, event.offsetY];
+        });
 
-canvas.addEventListener("mousemove", draw);
-canvas.addEventListener("mouseup", () => (isDrawing = false));
-canvas.addEventListener("mouseout", () => (isDrawing = false));
+        canvas.addEventListener("mousemove", (event) => {
+            if (!isDrawing) return;
+            ctx.strokeStyle = colorInput.value;
+            ctx.lineWidth = shapeSizeInput.value;
+            ctx.lineJoin = "round";
+            ctx.lineCap = "round";
+            ctx.beginPath();
+            ctx.moveTo(lastX, lastY);
+            ctx.lineTo(event.offsetX, event.offsetY);
+            ctx.stroke();
+            [lastX, lastY] = [event.offsetX, event.offsetY];
+        });
 
-function draw() {
-    if (!isDrawing) return;
-    ctx.strokeStyle = colorInput.value;
-    ctx.lineWidth = brushSizeInput.value;
-    ctx.lineJoin = "round";
-    ctx.lineCap = "round";
-    ctx.beginPath();
-    ctx.moveTo(lastX, lastY);
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
-    [lastX, lastY] = [event.offsetX, event.offsetY];
-}
+        // Agrega un evento de escucha al soltar el botón del mouse para detener el dibujo
+        canvas.addEventListener("mouseup", () => {
+            isDrawing = false;
+        });
 
-clearCanvasButton.addEventListener("click", () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-});
+        // Agrega un evento de escucha para salir del lienzo para detener el dibujo
+        canvas.addEventListener("mouseout", () => {
+            isDrawing = false;
+        });
 
-colorInput.addEventListener("input", () => {
-    ctx.strokeStyle = colorInput.value;
-});
+        clearCanvasButton.addEventListener("click", () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        });
 
-// Ejemplo de cómo dibujar una figura (círculo) en el lienzo
-function drawShape(x, y) {
-    const size = shapeSizeInput.value;
-    ctx.fillStyle = colorInput.value;
-    ctx.beginPath();
-    ctx.arc(x, y, size, 0, Math.PI * 2);
-    ctx.fill();
-}
-
-canvas.addEventListener("click", (event) => {
-    const x = event.offsetX;
-    const y = event.offsetY;
-    drawShape(x, y);
+        colorInput.addEventListener("input", () => {
+            ctx.strokeStyle = colorInput.value;
+        });
+    }
 });
--- src/main/webapp/WEB-INF/jsp/pagina.jsp
+++ src/main/webapp/WEB-INF/jsp/pagina.jsp
@@ -2,24 +2,30 @@
 <html>
 <head>
     <title>Register</title>
+    <link rel="stylesheet" type="text/css" href="/CSS/style.css">
+
+
 </head>
 <body>
     <h1>DIBUJO</h1>
     <canvas id="canvas" width="800" height="600"></canvas>
+    <script src="/JS/java.js"></script>
         <br>
         <br>
         <button id="clearCanvas">Limpiar</button>
-        <script src="java.js"></script>
+
+
 
     <form action="/pagina" method="post">
-        <label for="figura">Figure:</label>
-        <select id="figura" name="figura"
-                    <option value="square">Square</option>
-                    <option value="triangle">Triangle</option>
-                    <option value="star">Star</option>
-                    <option value="circle">Circle</option>
-
-                </select><br><br>
+       <label for="figura">Figure:</label>
+       <select id="figura" name="figura">
+           <option value="square">Square</option>
+           <option value="triangle">Triangle</option>
+           <option value="star">Star</option>
+           <option value="circle">Circle</option>
+       </select><br><br>
+ <label for="brush">Brush:</label>
+      <input type="checkbox" id="brush">
 
         <label for="Size">Size:</label>
         <input type="range" id="size" min="1" max="10" value="1">
